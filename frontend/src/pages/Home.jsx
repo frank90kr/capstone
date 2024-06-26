790d99a57a23d5ee583356ecfc322480297d25e6
Francesco Gentile <francescogentilekr@gmail.com>
2024-06-26 18:08:48 +0200

    add carousel in home for courses-card and more

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -20,6 +20,8 @@ import { useSelector } from "react-redux";
 import { FaQuoteLeft } from "react-icons/fa6";
 import { IoIosArrowDropleftCircle } from "react-icons/io";
 import { IoIosArrowDroprightCircle } from "react-icons/io";
+import { TiSocialInstagram } from "react-icons/ti";
+import { TiSocialGithub } from "react-icons/ti";
 
 const Home = () => {
   const [courses, setCourses] = useState([]);
@@ -93,6 +95,34 @@ const Home = () => {
       console.error("Error checking purchase status:", error);
     }
   };
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  const nextSlide = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % courses.length);
+  };
+
+  const prevSlide = () => {
+    setIndex((prevIndex) => (prevIndex - 1 + courses.length) % courses.length);
+  };
+
+  const chunkCourses = () => {
+    const courseChunks = [];
+    for (let i = 0; i < courses.length; i += 4) {
+      const chunk = courses.slice(i, i + 4);
+      if (chunk.length < 4) {
+        const remaining = 4 - chunk.length;
+        chunk.push(...courses.slice(0, remaining));
+      }
+      courseChunks.push(chunk);
+    }
+    return courseChunks;
+  };
+
+  const courseChunks = chunkCourses();
 
   return (
     <div className="body">
@@ -129,9 +159,7 @@ const Home = () => {
           </Col>
         </Row>
       </Container>
-
       {/* Under Hero */}
-
       <Container fluid className="under-hero" data-aos="fade-up" data-aos-duration="1000">
         <Row className="container-under-hero d-sm-flex justify-content-center align-items-center ms-3">
           <Col>
@@ -169,7 +197,6 @@ const Home = () => {
           </Col>
         </Row>
       </Container>
-
       {/* Section 2 */}
       <Container fluid className="section2 mt-5" data-aos="fade-right" data-aos-duration="2500">
         <Row className="align-items-start justify-content-center">
@@ -224,27 +251,39 @@ const Home = () => {
           </Col>
         </Row>
       </Container>
-
       {/* Section3 Card's Course */}
       <Container fluid>
         <h2 className="text-center py-4">I nostri corsi</h2>
-        <Row className="justify-content-center mt-5 gap-1">
-          {courses.map((course, index) => (
-            <Col key={course.id} md={3} className="mb-4">
-              <Card className="card border border-3 rounded-top border-light" onClick={() => handleOpenModal(course)}>
-                <Card.Img className="course-image rounded-top" variant="top" src={course.image} />
-                <Card.Body>
-                  <Card.Title className="text-center">{course.title}</Card.Title>
-                </Card.Body>
-                <Card.Footer>
-                  <p className="text-center mb-4">Creato da: {course.creator_name}</p>{" "}
-                  {/* Mostra il nome del creatore */}
-                  <p>Prezzo {course.price}</p>
-                </Card.Footer>
-              </Card>
-            </Col>
+        <Carousel
+          interval={null}
+          indicators={false}
+          prevIcon={<IoIosArrowDropleftCircle className="custom-arrow-cards prev-arrow-cards" />}
+          nextIcon={<IoIosArrowDroprightCircle className="custom-arrow-cards next-arrow-cards" />}
+          className="mt-5"
+          activeIndex={index}
+          onSelect={handleSelect}
+        >
+          {courseChunks.map((courseChunk, idx) => (
+            <Carousel.Item key={idx}>
+              <Row className="justify-content-center gap-2 mt-1">
+                {courseChunk.map((course) => (
+                  <Col key={course.id} md={2} className="cards-col mb-3 d-flex justify-content-center">
+                    <Card className="card border border-2 border-light mt-1" onClick={() => handleOpenModal(course)}>
+                      <Card.Img className="course-image rounded-top" variant="top" src={course.image} />
+                      <Card.Body>
+                        <Card.Title className="text-center">{course.title}</Card.Title>
+                      </Card.Body>
+                      <Card.Footer>
+                        <p className="text-center mb-4">Creato da: {course.creator_name}</p>
+                        <p>Prezzo: {course.price}</p>
+                      </Card.Footer>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </Carousel.Item>
           ))}
-        </Row>
+        </Carousel>
         <Modal show={showModal} onHide={handleCloseModal}>
           <Modal.Header closeButton>
             <Modal.Title>Corso: {selectedCourse?.title}</Modal.Title>
@@ -277,14 +316,12 @@ const Home = () => {
                 </Button>
               </Link>
             )}
-
             <Button className="login-button close border border-none" onClick={handleCloseModal}>
               Chiudi
             </Button>
           </Modal.Footer>
         </Modal>
       </Container>
-
       {/* Section 4 Quiz */}
       <Container fluid className="py-5" data-aos="fade-up" data-aos-duration="2000" loop={true}>
         <Row className="align-items-center align-content-center justify-content-center container-quiz-home text-white">
@@ -320,7 +357,6 @@ const Home = () => {
           </Col>
         </Row>
       </Container>
-
       {/* Section 5 Teacher */}
       <Container fluid className="section5 mt-5" data-aos="fade-in" data-aos-duration="2500">
         <Row className="align-items-start justify-content-center">
@@ -342,7 +378,6 @@ const Home = () => {
           </Col>
         </Row>
       </Container>
-
       <Container fluid className="section6 mt-5">
         <Row className="justify-content-center">
           <h2 className="text-center mt-1">Testimonials</h2>
@@ -409,7 +444,102 @@ const Home = () => {
                   Reiciendis, necessitatibus.
                 </p>
               </Carousel.Item>
+              <Carousel.Item className="text-center mt-4">
+                <img src="/home_img/the-rock.jpg" alt="Greg Brown" className="udemy-admin google-ceo" />
+                <h5 className="mt-3 fw-semibold">Dwayne Johnson "The Rock"</h5>
+                <h6 className="mt-3 fw-semibold text-secondary">Student/Actor/WWE Wrestler</h6>
+                <FaQuoteLeft className="quote-icon mt-2" />
+                <p className=" custom-paragraph">
+                  Recentemente ho riscoperto la mia passione per l'informatica. Grazie alla vostra piattaforma ho
+                  trovato il modo perfetto per coltivare questa passione, anche tra un set e l'altro. <br /> È
+                  incredibile come la tecnologia e la formazione possano essere accessibili ovunque e in qualsiasi
+                  momento. Grazie per aver reso possibile il mio viaggio verso il mondo digitale!
+                </p>
+              </Carousel.Item>
             </Carousel>
+          </Col>
+        </Row>
+      </Container>
+
+      <Container fluid>
+        <Row className="footer py-5 mt-5">
+          <Col xs={12} md={4}>
+            <small className="d-block mb-3">© 2024</small>
+          </Col>
+          <Col xs={6} md={2}>
+            <h5>Menu</h5>
+            <ul className="list-unstyled text-small">
+              <li>
+                <Link to="#" className="footer-link">
+                  Corsi
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="footer-link">
+                  Quiz
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="footer-link">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="footer-link">
+                  Contatti
+                </Link>
+              </li>
+            </ul>
+          </Col>
+          <Col xs={6} md={2}>
+            <h5>Link utili</h5>
+            <ul className="list-unstyled text-small">
+              <li>
+                <Link to="#" className="footer-link">
+                  Chi siamo
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="footer-link">
+                  FAQ
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="footer-link">
+                  Termini di servizio
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="footer-link">
+                  Privacy
+                </Link>
+              </li>
+            </ul>
+          </Col>
+          <Col xs={6} md={2}>
+            <h5>Partner</h5>
+            <ul className="list-unstyled text-small">
+              <li>
+                <Link to="#" className="footer-link">
+                  Google
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="footer-link">
+                  Udemy
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="footer-link">
+                  CodeCademy
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="footer-link">
+                  Meta
+                </Link>
+              </li>
+            </ul>
           </Col>
         </Row>
       </Container>
